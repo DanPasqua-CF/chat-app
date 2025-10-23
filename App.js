41f3381378a7df14b55a486bd8c9ae570f9d83a6
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-10-22 23:43:20 -0400

    WIP

--- App.js
+++ App.js
@@ -1,33 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-/* Screen imports */
-import Start from './components/Start';
-import Chat from './components/Chat';
-
-/* Import Firestore */
-import { db } from './firebase';
-
-/* Create the navigator */
-const Stack = createNativeStackNavigator();
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Start'>
-          {/* Start screen */}
-          <Stack.Screen name='Start' component={Start} />
-          
-          {/* Chat screen */}
-          <Stack.Screen name='Chat'>
-            {props => <Chat db={db} {...props} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style='auto' />
-    </SafeAreaProvider>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello Expo!</Text>
+    </View>
   );
 }
